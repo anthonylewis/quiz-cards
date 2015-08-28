324047452fe844df02f10ebedd79feec25717d35
Anthony Lewis <alewis@sharethrough.com>
2015-08-28 15:13:27 -0500

    More results, random

--- js/quizcards.js
+++ js/quizcards.js
@@ -5,5 +5,44 @@
     var quiz = this;
 
     quiz.state = 'title';
+    quiz.result = 0;
+
+    quiz.questions = [
+      {
+        title: '',
+        answers: [
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ],
+      },
+    ];
+
+    quiz.results = [
+      {
+        title: 'Mega Goth',
+        desc:  'The dark hair, clothes, nails, and other various accessories don’t fool anyone. Although you may look like you live in the underworld, you’re truly sweet and super caring.',
+        img:   'http://s3-ak.buzzfeed.com/static/2014-06/12/12/tmp/webdr04/733115ba061468eb7f5d8adf52fe396d-10.jpg'
+      },
+      {
+        title: 'Neo Pirate',
+        desc:  'You\'re a go-getter who\'s not afraid to take what you want... even if it belongs to someone else. You know your time in this life is limited, so you take every day as it comes and enjoy life on the edge!',
+        img:   'http://s3-ak.buzzfeed.com/static/2014-06/12/12/tmp/webdr05/038b6f6a5db653731481ffdc71d9710d-1.jpg'
+      },
+      {
+        title: 'Steampunk',
+        desc:  'You\'re the best of both worlds: old and new; wise, yet young; spirited, yet feeble. You appreciate tradition, but you love progress. You\'re very unique, a little quirky, and extremely lovable!',
+        img: 'http://s3-ak.buzzfeed.com/static/2014-06/12/12/tmp/webdr06/db645c826bd8cd45bdc35571328a61d0-6.jpg'
+      }
+    ];
+
+    quiz.getResult = function () {
+      quiz.state = 'result'
+      quiz.result = Math.floor(Math.random() * quiz.results.length);
+      console.log(quiz.result);
+    }
   });
 })();
